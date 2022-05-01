1029f26a587075d9edd659d392b32e742cfc99ce
James Fraley <JamesFraley@yahoo.com>
2022-05-01 09:36:47 -0400

    Initial commit of working React Boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a template for React application.  Many of the installed components are very old.  A massive update would help.
+To run the program:
+```
+yarn run dev-server
+```
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+//import './styles/styles.css';
+
+let app=document.getElementById('app');
+ReactDOM.render(<p>This is my boilerplate</p>, app);
